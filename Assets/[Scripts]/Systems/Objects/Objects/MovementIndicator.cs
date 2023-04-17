b2aa12055f40664d90d5d7d8dd1a27902bd0bed2
MaybeCanadian <1coffeye@gmail.com>
2023-04-17 14:35:28 -0400

    added a follow cursor function to the base UIObject

--- Assets/[Scripts]/Systems/Objects/Objects/MovementIndicator.cs
+++ Assets/[Scripts]/Systems/Objects/Objects/MovementIndicator.cs
@@ -4,47 +4,8 @@ using UnityEngine;
 
 public class MovementIndicator : UIObject
 {
-    //private bool followingMouseCursor = false;
-
     public MovementIndicator() : base(UIList.MovementIndicator)
     {
 
     }
-    //protected override void SetUpObject()
-    //{
-    //    objType = ObjectTypeFilters.UI;
-    //}
-    //protected override void ConnectEvents()
-    //{
-    //    base.ConnectEvents();
-    //}
-    //protected override void DisconnectEvents()
-    //{
-    //    base.DisconnectEvents();
-    //}
-    //public void FollowCursor()
-    //{
-    //    if(followingMouseCursor == true)
-    //    {
-    //        return;
-    //    }
-
-    //    followingMouseCursor = true;
-    //    GridNode.OnNodeEnter += OnNodeEnter;
-    //}
-    //public void StopFollowingCursor()
-    //{
-    //    if(followingMouseCursor == false)
-    //    {
-    //        return;
-    //    }
-
-    //    followingMouseCursor = false;
-    //    GridNode.OnNodeEnter -= OnNodeEnter;
-    //}
-
-    //private void OnNodeEnter(GridNode node) 
-    //{
-    //    PlaceObjectAtGridPos(node);
-    //}
 }
--- Assets/[Scripts]/Systems/Objects/UI/UIObject.cs
+++ Assets/[Scripts]/Systems/Objects/UI/UIObject.cs
@@ -5,11 +5,53 @@ using UnityEngine;
 public class UIObject : Object
 {
     UIList ui;
+    bool followingCursor = false;
 
+    #region Init Functions
     public UIObject(UIList uiType) : base(null)
     {
         ui = uiType;
     }
+    #endregion
+
+    #region UI Functions
+    public void FollowCursor()
+    {
+        if(followingCursor == false) 
+        {
+            InputController.OnMouseGridPosChanged += OnMouseGridPosChanged;
+
+            followingCursor = true;
+        }
+    }
+    public void StopFollowCursor()
+    {
+        if (followingCursor == true)
+        {
+            InputController.OnMouseGridPosChanged -= OnMouseGridPosChanged;
+
+            followingCursor = false;
+        }
+    }
+    #endregion
+
+    #region Event Receivers
+    private void OnMouseGridPosChanged()
+    {
+        if(followingCursor == true)
+        {
+            if(InputController.MouseGridNode != null)
+            {
+                PlaceObjectAtGridPos(InputController.MouseGridNode);
+                CreateVisuals();
+                return;
+            }
+
+            DestroyVisuals();
+            return;
+        }
+    }
+    #endregion
 
     #region Overrides
     //We do not want to add these to the node for now, maybe in the future we will change this
@@ -24,6 +66,10 @@ public class UIObject : Object
     protected override GameObject GetObjectModel()
     {
         return UIDataBase.GetUI(ui);
+    }
+    protected override void SetUpObject()
+    {
+        objType = ObjectTypeFilters.UI;
     }
     #endregion
 }
